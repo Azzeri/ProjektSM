50cd903b5247a36e313d69510c1ccefc817be412
Mariusz <artess2698@gmail.com>
2023-01-11 20:35:22 +0100

    recording audio

--- refactored.py
+++ refactored.py
@@ -23,51 +23,53 @@ DURATION_SECONDS = 5
 NO_TEST_SAMPLES = 2
 PASSWORDS = list(open('passwords.txt'))
 LOCALPATH = "C:\\Users\\artes\\OneDrive\\Semestr 9\\SM\\Projekt\\"
+SAMPLE_NUMBER_FOR_ONE_MODEL = 0
 
 
 class Identification:
     def __init__(self):
-        self.ws = self.initialize_window()
+        self.ws_main = self.initialize_window("Identyfikacja")
         self.recording_device = IntVar()
+        self.pyAudio = pyaudio.PyAudio()
         self.initialize_navigation()
         self.initialize_main_view()
-        self.ws.mainloop()
+        self.ws_main.mainloop()
 
-    def initialize_window(self):
+    def initialize_window(self, title):
         ws = Tk()
         ws.geometry("700x350")
-        ws.title("Identyfikacja użytkownika")
+        ws.title(title)
 
         return ws
 
     def initialize_navigation(self):
-        frame = Frame(self.ws)
+        frame = Frame(self.ws_main)
         frame.pack()
         mainmenu = Menu(frame)
-        mainmenu.add_command(label="Dodaj dane", command=self.add_model)
-        mainmenu.add_command(label="Trenuj model", command=self.train_models)
+        mainmenu.add_command(label="Dodaj dane", command=self.nav_add_model)
+        mainmenu.add_command(label="Trenuj model",
+                             command=self.nav_train_models)
         mainmenu.add_command(
-            label="Identifikuj nagrane próbki", command=self.identify_samples)
-        mainmenu.add_command(label="Exit", command=self.ws.destroy)
-        self.ws.config(menu=mainmenu)
+            label="Identifikuj nagrane próbki", command=self.nav_identify_samples)
+        mainmenu.add_command(label="Exit", command=self.ws_main.destroy)
+        self.ws_main.config(menu=mainmenu)
 
     def initialize_main_view(self):
         self.initialize_recording_devices()
-
         label_title = Label(
-            self.ws, text="Identyfikacja użytkownika", font='Arial 18')
+            self.ws_main, text="Identyfikacja użytkownika", font='Arial 18')
         label_title.place(relx=0.5, rely=0.10, anchor=CENTER)
 
         label_info = Label(
-            self.ws, text="Wciśnij przycisk i wypowiedź wyświetlone poniżej hasło:", font='Arial 15')
+            self.ws_main, text="Wciśnij przycisk i wypowiedź wyświetlone poniżej hasło:", font='Arial 15')
         label_info.place(relx=0.5, rely=0.4, anchor=CENTER)
 
-        label_password = Label(self.ws, text=random.choice(
+        label_password = Label(self.ws_main, text=random.choice(
             PASSWORDS), font='Arial 17 bold')
         label_password.place(relx=0.5, rely=0.6, anchor=CENTER)
 
         start_btn = Button(
-            self.ws,
+            self.ws_main,
             text='START',
             bd='5',
             command=self.record_sample
@@ -82,7 +84,7 @@ class Identification:
         # self.recording_device.set(1)
 
         for i in range(len(recordingDevicesList)):
-            Radiobutton(self.ws,
+            Radiobutton(self.ws_main,
                         text=recordingDevicesList[i],
                         padx=20,
                         variable=self.recording_device,
@@ -99,15 +101,96 @@ class Identification:
 
         return list_of_devices
 
-    def add_model(self):
-        print('here will add new model')
-        return
+    def add_model(self, ws_add_model, modelName, label_password, label_record):
+        global SAMPLE_NUMBER_FOR_ONE_MODEL
 
-    def train_models(self):
+        stream = self.pyAudio.open(
+            format=FORMAT,
+            channels=CHANNELS,
+            rate=RATE,
+            input=True,
+            input_device_index=self.recording_device.get(),
+            frames_per_buffer=CHUNK
+        )
+
+        label_password.place_forget()
+        label_password = Label(
+            ws_add_model, text=PASSWORDS[SAMPLE_NUMBER_FOR_ONE_MODEL], font='Arial 17 bold')
+        label_password.place(relx=0.5, rely=0.6, anchor=CENTER)
+
+        label_record = Label(ws_add_model, text="Nagrywam...", font='Arial 15')
+        label_record.place(relx=0.5, rely=0.25, anchor=CENTER)
+        ws_add_model.update()
+
+        frames = []
+        for i in range(0, int(RATE / CHUNK * DURATION_SECONDS)):
+            data = stream.read(CHUNK)
+            frames.append(data)
+
+        label_record.place_forget()
+        label_record = Label(
+            ws_add_model, text="Koniec nagrania...", font='Arial 15')
+        label_record.place(relx=0.5, rely=0.25, anchor=CENTER)
+        stream.stop_stream()
+        stream.close()
+        self.pyAudio.terminate()
+
+        OUTPUT_FILENAME = modelName + "-sample" + \
+            str(SAMPLE_NUMBER_FOR_ONE_MODEL) + ".wav"
+        WAVE_OUTPUT_FILENAME = os.path.join("training_set", OUTPUT_FILENAME)
+        trainedfilelist = open("training_set_addition.txt", 'a')
+        trainedfilelist.write(OUTPUT_FILENAME+"\n")
+        waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+        waveFile.setnchannels(CHANNELS)
+        waveFile.setsampwidth(self.pyAudio.get_sample_size(FORMAT))
+        waveFile.setframerate(RATE)
+        waveFile.writeframes(b''.join(frames))
+        waveFile.close()
+
+        time.sleep(1)
+        label_record.place_forget()
+        label_password.place_forget()
+        SAMPLE_NUMBER_FOR_ONE_MODEL += 1
+
+    def nav_add_model(self):
+        ws_add_model = self.initialize_window("Nowy model")
+
+        label_title = Label(
+            ws_add_model, text="Dodawanie profilu użytkownika", font='Arial 18')
+        label_title.place(relx=0.5, rely=0.10, anchor=CENTER)
+
+        modelName = StringVar()
+
+        name_label = Label(ws_add_model, text='Nazwa użytkownika',
+                           font=('calibre', 10, 'bold'))
+        name_entry = Entry(ws_add_model, textvariable=modelName,
+                           font=('calibre', 10, 'normal'))
+        name_label.place(relx=0.4, rely=0.3, anchor=CENTER)
+        name_entry.place(relx=0.6, rely=0.3, anchor=CENTER)
+
+        label_info = Label(
+            ws_add_model, text="Wciśnij przycisk i wypowiedź wyświetlone poniżej hasło:", font='Arial 15')
+        label_info.place(relx=0.5, rely=0.4, anchor=CENTER)
+
+        label_password = Label(
+            ws_add_model, text=PASSWORDS[0], font='Arial 17 bold')
+        label_password.place(relx=0.5, rely=0.6, anchor=CENTER)
+
+        start_btn = Button(
+            ws_add_model,
+            text='START',
+            bd='5',
+            command=lambda: self.add_model(
+                ws_add_model, name_entry.get(), label_password, label_info)
+        )
+
+        start_btn.place(relx=0.5, rely=0.7, anchor=CENTER)
+
+    def nav_train_models(self):
         print('here will train models')
         return
 
-    def identify_samples(self):
+    def nav_identify_samples(self):
         print('here will identify samples')
         return
 
