613171e060eedd5c5b481e2713b5e6be091ddf69
Mariusz <artess2698@gmail.com>
2023-01-11 20:59:44 +0100

    gmm training implemented

--- refactored.py
+++ refactored.py
@@ -101,6 +101,39 @@ class Identification:
 
         return list_of_devices
 
+    def calculate_delta(self, array):
+        rows, cols = array.shape
+        print(rows)
+        print(cols)
+        deltas = np.zeros((rows, 20))
+        N = 2
+        for i in range(rows):
+            index = []
+            j = 1
+            while j <= N:
+                if i-j < 0:
+                    first = 0
+                else:
+                    first = i-j
+                if i+j > rows-1:
+                    second = rows-1
+                else:
+                    second = i+j
+                index.append((second, first))
+                j += 1
+            deltas[i] = (array[index[0][0]]-array[index[0][1]] +
+                         (2 * (array[index[1][0]]-array[index[1][1]]))) / 10
+        return deltas
+
+    def extract_features(self, audio, rate):
+        mfcc_feature = mfcc.mfcc(audio, rate, 0.025, 0.01,
+                                 20, nfft=1200, appendEnergy=True)
+        mfcc_feature = preprocessing.scale(mfcc_feature)
+        print(mfcc_feature)
+        delta = self.calculate_delta(mfcc_feature)
+        combined = np.hstack((mfcc_feature, delta))
+        return combined
+
     def add_model(self, ws_add_model, modelName, label_password, label_record):
         global SAMPLE_NUMBER_FOR_ONE_MODEL
 
@@ -187,8 +220,38 @@ class Identification:
         start_btn.place(relx=0.5, rely=0.7, anchor=CENTER)
 
     def nav_train_models(self):
-        print('here will train models')
-        return
+        source = LOCALPATH + "training_set\\"
+        dest = LOCALPATH + "trained_models\\"
+        train_file = LOCALPATH + "training_set_addition.txt"
+        file_paths = open(train_file, 'r')
+        count = 1
+        features = np.asarray(())
+        for path in file_paths:
+            path = path.strip()
+            print(path)
+
+            sr, audio = read(source + path)
+            print(sr)
+            vector = self.extract_features(audio, sr)
+
+            if features.size == 0:
+                features = vector
+            else:
+                features = np.vstack((features, vector))
+
+            if count == 5:
+                gmm = GaussianMixture(
+                    n_components=6, max_iter=200, covariance_type='diag', n_init=3)
+                gmm.fit(features)
+
+                # dumping the trained gaussian model
+                picklefile = path.split("-")[0]+".gmm"
+                pickle.dump(gmm, open(dest + picklefile, 'wb'))
+                print('+ modeling completed for speaker:', picklefile,
+                      " with data point = ", features.shape)
+                features = np.asarray(())
+                count = 0
+            count = count + 1
 
     def nav_identify_samples(self):
         print('here will identify samples')
